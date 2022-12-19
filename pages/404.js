9fe99abd8358efa57b98c2076895d56b5bb3bf0a
MamadK9 <mohammad.kp8093@gmail.com>
2022-12-19 19:54:29 +0330

    404 modified

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import { BlobButton } from "components";
 const Page404 = () => {
 	return (
 		<div className='not-found-page'>
-			<h3>صفحه مورد نظر پیدا نشد! 😞</h3>
+			<h3> 😞!صفحه مورد نظر پیدا نشد</h3>
 			<BlobButton backgroundColor='#14a76c' isLink link={"/"} isTargetBlank={false}>
 				برگشت به صفحه اصلی
 			</BlobButton>
